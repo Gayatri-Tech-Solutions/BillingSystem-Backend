adc72b7c4f650c8ee947fec90948b002e1099b7f
Gayatri-Tech-Solutions <techsolutions.gayatri@gmail.com>
2024-09-08 12:02:20 +0530

    Update server.js cors url

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import routes from './routes/index.js'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import routes from './routes/index.js';
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use('/api', routes)
+const app = express();
 
 
-const PORT = process.env.PORT || 5000
+const allowedOrigins = process.env.FRONTEND_URLS.split(',');
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      // Allow requests from allowed origins or requests without an origin header (e.g., from non-browser clients)
+      callback(null, true);
+    } else {
+      // Block requests from other origins
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+app.use(express.json());
+app.use('/api', routes);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server Running on port ${PORT}`)
-})
+  console.log(`Server Running on port ${PORT}`);
+});
